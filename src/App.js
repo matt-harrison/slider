1ee62002a72bec78eeb28506d4c6a3165c793a3e
Matt Harrison <matthew.harrison@homes.com>
2019-04-11 15:24:21 -0400

    replaced deprecated componentWillMount with constructor. added resize debounce. added multislide. added margin. hacked transition toggle on resize.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,43 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  state = {
-    backgroundHeight: 1519,
-    backgroundWidth: 1000,
-    columns: 4,
-    img: 'http://rootbeercomics.com/illustration/posters/img/flickit-fridays.jpg',
-    puzzleHeight: null,
-    puzzleWidth: null,
-    rows: 6,
-    squareWidth: null,
-    squareHeight: null,
-    squares: []
-  }
+  constructor(props) {
+    super(props);
 
-  componentWillMount() {
-    this.formatPuzzle(true);
-
-    window.addEventListener('resize', () => {this.formatPuzzle()});
-  }
-
-  buildPuzzle = () => {
     let squares   = [];
     let positions = [];
+    let initialState = {
+      backgroundHeight: 1519,
+      backgroundWidth: 1000,
+      columns: 4,
+      isResizing: false,
+      img: 'http://rootbeercomics.com/illustration/posters/img/flickit-fridays.jpg',
+      puzzleHeight: null,
+      puzzleWidth: null,
+      rows: 6,
+      squareWidth: null,
+      squareHeight: null,
+      squares,
+      debounceTimer: {}
+    };
 
-    for (let row = 0; row < this.state.rows; row++) {
-      for (let column = 0; column < this.state.columns; column++) {
-        const id = row * this.state.columns + column;
+    initialState = Object.assign(initialState, this.formatPuzzle(initialState));
+
+    for (let row = 0; row < initialState.rows; row++) {
+      for (let column = 0; column < initialState.columns; column++) {
+        const id = row * initialState.columns + column;
         const position = {
           column,
           row
         };
         const square = {
-          backgroundPosition: {
+          originalPosition: {
             column,
             row
           },
           style: {
-            opacity: (id === (this.state.columns * this.state.rows) - 1) ? '0'  : '1',
-            zIndex:  (id === (this.state.columns * this.state.rows) - 1) ? '-1' : id
+            opacity: (id === (initialState.columns * initialState.rows) - 1) ? '0'  : '1',
+            zIndex:  (id === (initialState.columns * initialState.rows) - 1) ? '-1' : id
           }
         };
 
@@ -49,54 +48,86 @@ class App extends Component {
 
     squares = this.shuffleSquares(squares, positions);
 
-    this.setState({squares});
+    this.state = initialState;
+
+    window.addEventListener('resize', () => {
+      let debounceTimer = this.state.debounceTimer;
+
+      clearTimeout(debounceTimer);
+
+      debounceTimer = setTimeout(this.debounceFormatPuzzle, 250);
+
+      this.setState({debounceTimer});
+    });
   }
 
-  formatPuzzle = (isInit = false) => {
-    const backgroundRatio = this.state.backgroundWidth / this.state.backgroundHeight;
-    const isFullWidth     = window.innerWidth / backgroundRatio <= window.innerHeight;
-    const puzzleWidth     = (isFullWidth) ? Math.floor(window.innerWidth) : Math.floor(window.innerHeight * backgroundRatio);
-    const puzzleHeight    = Math.floor(puzzleWidth / backgroundRatio);
-    const puzzleScale     = Math.floor(puzzleWidth / this.state.backgroundWidth);
-    const squareWidth     = Math.floor(puzzleWidth / this.state.columns);
-    const squareHeight    = Math.floor(puzzleHeight / this.state.rows);
+  debounceFormatPuzzle = () => {
+    const format = this.formatPuzzle(this.state);
 
     this.setState({
+      isResizing: true,
+      ...format
+    }, () => {
+      setTimeout(() => {
+        this.setState({isResizing: false}, () => {
+        });
+      }, 100);
+    });
+  };
+
+  formatPuzzle = (state) => {
+    const windowWidth     = window.innerWidth - 20;
+    const windowHeight    = window.innerHeight - 20;
+    const backgroundRatio = state.backgroundWidth / state.backgroundHeight;
+    const isFullWidth     = windowWidth / backgroundRatio <= windowHeight;
+    const puzzleWidth     = (isFullWidth) ? windowWidth : windowHeight * backgroundRatio;
+    const puzzleHeight    = puzzleWidth / backgroundRatio;
+    const puzzleScale     = puzzleWidth / state.backgroundWidth;
+    const squareWidth     = puzzleWidth / state.columns;
+    const squareHeight    = puzzleHeight / state.rows;
+
+    return {
       puzzleHeight,
       puzzleScale,
       puzzleWidth,
       squareHeight,
       squareWidth
-    }, () => {
-      if (isInit) {
-        this.buildPuzzle();
-      }
-    });
+    };
   };
 
   handleClick = (index) => {
-    let squares = this.state.squares.slice();
-    const empty = this.state.squares[this.state.squares.length - 1];
+    let   clicked       = this.state.squares[index];
+    const clickedColumn = clicked.column;
+    const clickedRow    = clicked.row;
+    let   empty         = this.state.squares[this.state.squares.length - 1];
+    const emptyColumn   = empty.column;
+    const emptyRow      = empty.row;
+    let   squares       = this.state.squares.slice();
 
-    if (empty.row === squares[index].row) {
-      if (empty.column === squares[index].column + 1) {
-        squares[index].column++;
-        empty.column--;
-      } else if (empty.column === squares[index].column - 1) {
-        squares[index].column--;
-        empty.column++;
-      }
-    } else if (empty.column === squares[index].column) {
-      if (empty.row === squares[index].row + 1) {
-        squares[index].row++;
-        empty.row--;
-      } else if (empty.row === squares[index].row - 1) {
-        squares[index].row--;
-        empty.row++;
-      }
+    if (clickedColumn === emptyColumn && clickedRow !== emptyRow || (clickedRow === emptyRow && clickedColumn !== emptyColumn)) {
+      squares.map((square) => {
+        if (!(square.column === emptyColumn && square.row === emptyRow)) {
+          if (emptyColumn === clickedColumn && square.column === clickedColumn) {
+            if (emptyRow > clickedRow && square.row >= clickedRow && square.row < emptyRow) {
+              square.row++;
+            } else if (emptyRow < clickedRow && square.row <= clickedRow && square.row > emptyRow) {
+              square.row--;
+            }
+          } else if (emptyRow === clickedRow && square.row === clickedRow) {
+            if (emptyColumn > clickedColumn && square.column >= clickedColumn && square.column < emptyColumn) {
+              square.column++;
+            } else if (emptyColumn < clickedColumn && square.column <= clickedColumn && square.column > emptyColumn) {
+              square.column--;
+            }
+          }
+        } else {
+          square.column = clickedColumn;
+          square.row    = clickedRow;
+        }
+      });
+
+      this.setState({squares});
     }
-
-    this.setState({squares});
   };
 
   shuffleSquares = (squares, positions) => {
@@ -113,48 +144,48 @@ class App extends Component {
 
   render() {
     return (
+      <div
+      className="puzzle"
+      style={{
+        width:  this.state.puzzleWidth,
+        height: this.state.puzzleHeight,
+      }}
+      >
         <div
-        className="puzzle"
+        className="background"
         style={{
-          width:  this.state.puzzleWidth,
-          height: this.state.puzzleHeight,
+          backgroundImage: `url(${this.state.img})`,
+          backgroundSize:  `${this.state.puzzleWidth}px ${this.state.puzzleHeight}px`
         }}
-        >
-          <div
-          className="background"
-          style={{
+        />
+        {this.state.squares.map((square, index) => {
+          const x     = square.originalPosition.column * this.state.squareWidth;
+          const y     = square.originalPosition.row * this.state.squareHeight;
+          const style = {
+            top: square.row * this.state.squareHeight,
+            left: square.column * this.state.squareWidth,
+            width: this.state.squareWidth,
+            height: this.state.squareHeight,
+            backgroundPosition: `${(0 - x)}px ${(0 - y)}px`,
             backgroundImage: `url(${this.state.img})`,
-            backgroundSize:  `${this.state.puzzleWidth}px ${this.state.puzzleHeight}px`
-          }}
-          />
-          {this.state.squares.map((square, index) => {
-            const x     = square.backgroundPosition.column * this.state.squareWidth;
-            const y     = square.backgroundPosition.row * this.state.squareHeight;
-            const style = {
-              top: square.row * this.state.squareHeight,
-              left: square.column * this.state.squareWidth,
-              width: this.state.squareWidth,
-              height: this.state.squareHeight,
-              backgroundPosition: `${(0 - x)}px ${(0 - y)}px`,
-              backgroundImage: `url(${this.state.img})`,
-              backgroundSize: `${this.state.puzzleWidth}px ${this.state.puzzleHeight}px`,
-              opacity: square.style.opacity,
-              zIndex:  square.style.zIndex
-            };
+            backgroundSize: `${this.state.puzzleWidth}px ${this.state.puzzleHeight}px`,
+            opacity: square.style.opacity,
+            zIndex:  square.style.zIndex
+          };
 
-            return (
-              <div
-              className="square"
-              key={index}
-              style={style}
-              onClick={(e) => {
-                e.preventDefault();
-                this.handleClick(index);
-              }}
-              />
-            );
-          })}
-        </div>
+          return (
+            <div
+            className={this.state.isResizing ? 'square hotdog' : 'square trans'}
+            key={index}
+            style={style}
+            onClick={(e) => {
+              e.preventDefault();
+              this.handleClick(index);
+            }}
+            />
+          );
+        })}
+      </div>
     );
   }
 }
